dfc64a777668bc33066fb88ba26ad75a3179367b
Jamie Pinheiro <jamiepinheiro@gmail.com>
2018-04-07 21:49:33 -0400

    Centered text

--- src/FindChat.js
+++ src/FindChat.js
@@ -7,13 +7,11 @@ class FindChat extends Component {
     super(props);
     this.nameChange = this.nameChange.bind(this);
     this.findChat = this.findChat.bind(this);
-    pingServer();
-  }
-
-  async pingServer() {
-      try{
-         await axios.get("https://gifchat1server.herokuapp.com/"); 
-      }
+    try{
+        axios.get("https://gifchat1server.herokuapp.com/");
+    }catch (e) {
+        console.log(e);
+    }
   }
 
   nameChange(event) {
@@ -41,7 +39,7 @@ class FindChat extends Component {
         <form className="form-group mx-auto text-center col-8" onSubmit={this.findChat}>
             {this.props.page === 'findChat' &&
 				<div className="form-group">
-					<input className="form-control" type="message" placeholder="what's your name?" value={this.props.name} onChange={this.nameChange} autoFocus/>
+					<input className="form-control text-center" type="message" placeholder="What's your name?" value={this.props.name} onChange={this.nameChange} autoFocus/>
 				</div>}
             <button className={"btn btn-primary col-8 " + (this.props.name.length > 0 ? '' : 'disabled')} type="submit">Find a Chat</button>
         </form>
